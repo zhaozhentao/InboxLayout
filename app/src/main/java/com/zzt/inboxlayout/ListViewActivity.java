8fae03a92a3071ca5f57050f90be71d93f8e6646
招振涛 <514656282@qq.com>
2015-02-01 17:50:00 +0800

    增加MainActivity

---
+++ app/src/main/java/com/zzt/inboxlayout/ListViewActivity.java
@@ -0,0 +1,128 @@
+package com.zzt.inboxlayout;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+
+import com.zzt.inbox.interfaces.OnDragStateChangeListener;
+import com.zzt.inbox.widget.InboxLayoutBase;
+import com.zzt.inbox.widget.InboxLayoutListView;
+import com.zzt.inbox.widget.InboxBackgroundScrollView;
+
+
+/**
+ * Created by zzt on 2015/1/19.
+ */
+public class ListViewActivity extends ActionBarActivity {
+    InboxLayoutListView inboxLayoutListView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xdd000000));
+
+        final InboxBackgroundScrollView inboxBackgroundScrollView = (InboxBackgroundScrollView)findViewById(R.id.scroll);
+        inboxLayoutListView = (InboxLayoutListView)findViewById(R.id.inboxlayout);
+        inboxLayoutListView.seBackgroundScrollView(inboxBackgroundScrollView);//绑定scrollview
+        inboxLayoutListView.setCloseDistance(50);
+        inboxLayoutListView.setOnDragStateChangeListener(new OnDragStateChangeListener() {
+            @Override
+            public void dragStateChange(InboxLayoutBase.DragState state) {
+                switch (state) {
+                    case CANCLOSE:
+                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xff5e5e5e));
+                        getSupportActionBar().setTitle("back");
+                        break;
+                    case CANNOTCLOSE:
+                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xdd000000));
+                        getSupportActionBar().setTitle("InboxLayout");
+                        break;
+                }
+            }
+        });
+        inboxLayoutListView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 20;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                LayoutInflater inflater = ListViewActivity.this.getLayoutInflater();
+                View view = inflater.inflate(R.layout.item, null);
+                return view;
+            }
+        });
+
+        init();
+    }
+
+
+    private void init() {
+        final LinearLayout dingdan = (LinearLayout)findViewById(R.id.ding_dan);
+        dingdan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inboxLayoutListView.openWithAnim(dingdan);
+            }
+        });
+
+        final LinearLayout yuding = (LinearLayout)findViewById(R.id.yuding);
+        yuding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inboxLayoutListView.openWithAnim(yuding);
+            }
+        });
+
+        final LinearLayout tuijian = (LinearLayout)findViewById(R.id.tuijian);
+        tuijian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inboxLayoutListView.openWithAnim(tuijian);
+            }
+        });
+
+        final LinearLayout member = (LinearLayout)findViewById(R.id.member);
+        member.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inboxLayoutListView.openWithAnim(member);
+            }
+        });
+
+        final LinearLayout choujiang = (LinearLayout)findViewById(R.id.choujiang);
+        choujiang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inboxLayoutListView.openWithAnim(choujiang);
+            }
+        });
+
+        final LinearLayout diyongquan = (LinearLayout)findViewById(R.id.diyongquan);
+        diyongquan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inboxLayoutListView.openWithAnim(diyongquan);
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/zzt/inboxlayout/MainActivity.java
+++ app/src/main/java/com/zzt/inboxlayout/MainActivity.java
@@ -1,128 +1,32 @@
 package com.zzt.inboxlayout;
 
-import android.graphics.drawable.ColorDrawable;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
-
-import com.zzt.inbox.interfaces.OnDragStateChangeListener;
-import com.zzt.inbox.widget.InboxLayoutBase;
-import com.zzt.inbox.widget.InboxLayoutListView;
-import com.zzt.inbox.widget.InboxBackgroundScrollView;
-
+import android.widget.Button;
 
 /**
- * Created by zzt on 2015/1/19.
+ * Created by zzt on 2015/2/1.
  */
-public class MainActivity extends ActionBarActivity {
-    InboxLayoutListView inboxLayoutListView;
+public class MainActivity extends ActionBarActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xdd000000));
-
-        final InboxBackgroundScrollView inboxBackgroundScrollView = (InboxBackgroundScrollView)findViewById(R.id.scroll);
-        inboxLayoutListView = (InboxLayoutListView)findViewById(R.id.inboxlayout);
-        inboxLayoutListView.seBackgroundScrollView(inboxBackgroundScrollView);//绑定scrollview
-        inboxLayoutListView.setCloseDistance(50);
-        inboxLayoutListView.setOnDragStateChangeListener(new OnDragStateChangeListener() {
-            @Override
-            public void dragStateChange(InboxLayoutBase.DragState state) {
-                switch (state) {
-                    case CANCLOSE:
-                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xff5e5e5e));
-                        getSupportActionBar().setTitle("back");
-                        break;
-                    case CANNOTCLOSE:
-                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xdd000000));
-                        getSupportActionBar().setTitle("InboxLayout");
-                        break;
-                }
-            }
-        });
-        inboxLayoutListView.setAdapter(new BaseAdapter() {
-            @Override
-            public int getCount() {
-                return 20;
-            }
-
-            @Override
-            public Object getItem(int position) {
-                return null;
-            }
-
-            @Override
-            public long getItemId(int position) {
-                return position;
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                LayoutInflater inflater = MainActivity.this.getLayoutInflater();
-                View view = inflater.inflate(R.layout.item, null);
-                return view;
-            }
-        });
-
-        init();
-    }
-
-
-    private void init() {
-        final LinearLayout dingdan = (LinearLayout)findViewById(R.id.ding_dan);
-        dingdan.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);
+        Button btn1 = (Button)findViewById(R.id.listview);
+        Button btn2 = (Button)findViewById(R.id.scrollview);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                inboxLayoutListView.openWithAnim(dingdan);
+                startActivity(new Intent(MainActivity.this, ListViewActivity.class));
             }
         });
-
-        final LinearLayout yuding = (LinearLayout)findViewById(R.id.yuding);
-        yuding.setOnClickListener(new View.OnClickListener() {
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                inboxLayoutListView.openWithAnim(yuding);
-            }
-        });
-
-        final LinearLayout tuijian = (LinearLayout)findViewById(R.id.tuijian);
-        tuijian.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inboxLayoutListView.openWithAnim(tuijian);
-            }
-        });
-
-        final LinearLayout member = (LinearLayout)findViewById(R.id.member);
-        member.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inboxLayoutListView.openWithAnim(member);
-            }
-        });
-
-        final LinearLayout choujiang = (LinearLayout)findViewById(R.id.choujiang);
-        choujiang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inboxLayoutListView.openWithAnim(choujiang);
-            }
-        });
-
-        final LinearLayout diyongquan = (LinearLayout)findViewById(R.id.diyongquan);
-        diyongquan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inboxLayoutListView.openWithAnim(diyongquan);
+                startActivity(new Intent(MainActivity.this, ScrollViewActivity.class));
             }
         });
     }
-
-
 }
